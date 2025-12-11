ea6f90683d1c2732d223d3eb6b169435339d1c71
Fox-Demon-605 <slava-vasilev-2005@bk.ru>
2025-12-11 10:34:35 +0300

    Delete backend directory

--- backend/app/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-logger = logging.getLogger("library_app")
-handler = logging.StreamHandler()
-fmt = logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
-handler.setFormatter(fmt)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
